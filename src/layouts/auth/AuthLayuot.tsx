c9bde54d69ed9bf84daa0c70704279c2024cc501
Lenshmidt Andrei <lenshmidt.andrei@gmail.com>
2025-02-13 18:02:21 +0300

    feat: добавлен Dashboard

--- src/layouts/auth/AuthLayuot.tsx
+++ src/layouts/auth/AuthLayuot.tsx
@@ -1,38 +1,47 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router";
+import { type Navigation } from "@toolpad/core/AppProvider";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { ReactRouterAppProvider } from "@toolpad/core/react-router";
 
 export default function AuthLayout() {
+  const NAVIGATION: Navigation = [
+    {
+      kind: "header",
+      title: "Main items",
+    },
+    {
+      title: "Dashboard",
+      icon: <DashboardIcon />,
+    },
+    {
+      segment: "test",
+      title: "Orders",
+      icon: <ShoppingCartIcon />,
+    },
+  ];
+
+  const BRANDING = {
+    title: "My Toolpad Core App",
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <Box
-        sx={{
-          padding: "20px 0",
-          width: "100%",
-          textAlign: "center",
-          backgroundColor: "#a5d2ff",
-        }}
-        component={"header"}
-      >
-        <img src="#" alt="logo" />
-      </Box>
-      <Outlet />
-      <Box
-        sx={{
-          padding: "20px 0",
-          width: "100%",
-          textAlign: "center",
-          backgroundColor: "#a5d2ff",
-        }}
-        component={"footer"}
-      ></Box>
-    </Box>
+    <ReactRouterAppProvider navigation={NAVIGATION} branding={BRANDING}>
+      <DashboardLayout>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "calc(100vh - 56px)",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Outlet />
+        </Box>
+      </DashboardLayout>
+    </ReactRouterAppProvider>
   );
 }
