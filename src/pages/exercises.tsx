698a4eb8f3f880a7bfc03de528479eab538f05de
Lenshmidt Andrei <lenshmidt.andrei@gmail.com>
2025-02-21 17:24:41 +0300

    feat: добавлены данные

--- src/pages/exercises.tsx
+++ src/pages/exercises.tsx
@@ -1,5 +1,11 @@
 import Typography from "@mui/material/Typography";
+import ExersiseList from "../components/exersiseCompts/exersises";
 
 export default function ExersisesPage() {
-  return <Typography>Welcome to the ExersisesPage!</Typography>;
+  return (
+    <>
+      <Typography>Welcome to the ExersisesPage!</Typography>
+      <ExersiseList />
+    </>
+  );
 }
